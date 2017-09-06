aa35b04749dfac5d9cee11e4b346de5c4af3cf5f
kmui2 <kmui2@wisc.edu>
2017-09-06 12:21:04 -0500

    adding acao

--- index.js
+++ index.js
@@ -16,16 +16,22 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.set('port', (process.env.PORT || 7070))
 
-/**
- * Allow cross origin to access our /public directory from any site.
- * Make sure this header option is defined before defining of static path to /public directory
- */
-expressApp.use('/public',function(req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
     // Request headers you wish to allow
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
     // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
+
     // Pass to next layer of middleware
     next();
 });
