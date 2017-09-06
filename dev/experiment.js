c0b79865c34f174d6e96d2655c673c7f4a451f05
kmui2 <kmui2@wisc.edu>
2017-09-06 12:38:04 -0500

    adding port to http request

--- dev/experiment.js
+++ dev/experiment.js
@@ -125,7 +125,7 @@ function runExperiment(trials, subjCode, workerId, assignmentId, hitId) {
 
                 // POST response data to server
                 $.ajax({
-                    url: 'http://'+location.host+'/data',
+                    url: 'http://'+document.domain+':7070/data',
                     type: 'POST',
                     contentType: 'application/json',
                     data: JSON.stringify(response),
