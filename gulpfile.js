057272488f0e2f7805229cbafc4bd1fe81a6973c
kmui2 <kmui2@wisc.edu>
2017-09-06 23:40:04 -0500

    adding prod to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,21 @@
 const gulp = require('gulp');
+const htmlreplace = require('gulp-html-replace');
 
-gulp.task('hello', function() {
+gulp.task('default', function() {
     console.log("hello world");
 })
 
-gulp.task('prod', function() {
+gulp.task('copy', function() {
     gulp.src('dev/*')
         .pipe(gulp.dest('prod'));
 })
+
+gulp.task('switchjs', ['copy'], function() {
+    gulp.src('dev/index.html')
+        .pipe(htmlreplace({
+            'js': 'prod.js'
+        }))
+        .pipe(gulp.dest('prod'));
+})
+
+gulp.task('prod', ['copy', 'switchjs']);
